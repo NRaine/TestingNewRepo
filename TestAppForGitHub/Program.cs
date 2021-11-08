d7f2d0c7d76fb08e584cfc8954b2b6180e179352
Nadia Raine <nadia.raine@gavurin.com>
2021-11-08 15:47:26 +0000

    My 2nd change to this solution

--- TestAppForGitHub/Program.cs
+++ TestAppForGitHub/Program.cs
@@ -8,6 +8,7 @@ namespace TestAppForGitHub
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("This is a test app to see if Github works!");
+            Console.WriteLine("I've added a 2nd change to this file!");
         }
     }
 }
